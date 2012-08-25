2f83b3654f0d21c33b8e1daedded31be3382e4e6
weroth <weroth@gmail.com>
2012-08-25 23:16:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameHp::Application.config.secret_token = '9615ef5946cccb640e29f578abc207d0ceef420381ea82908d7cfed6e08343c74ad79be5b5de82b5da3bcd06641c8cd0d3ca6d3b34d1f311011c649aa7afbd3c'
