acc802b8c6759ba2609c52e7fc0bb52ac23d9e43
weroth <weroth@gmail.com>
2012-10-31 21:28:42 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+  end
+  
+  def show
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to Michey Soft!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
